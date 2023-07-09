8770002f6730a50fa63f8c0f611d8e40803c105c
marleyrosario <70246062+marleyrosario@users.noreply.github.com>
2023-07-09 13:56:44 -0500

    push

--- trends_app.py
+++ trends_app.py
@@ -1,44 +1,26 @@
 import streamlit as st
 import pandas as pd
-import openai
-from pytrends.request import TrendReq
-import os
-from dotenv import load_dotenv
 
-
-openai.api_key = os.getenv('openai_api_key')
-
-def description(json, keyword):
-    chat_log = []
-    json = str(json)[:2000]
-    system = {"role": "system", "content": f"{json}"}
-    chat_log.append(system)
-    prompt = {"role": "user", "content": "This data is going to displayed on a streamlit app.  Please return a 100 word description of the results of this Google Trends data from this keyword: " + keyword + "."}
-    chat_log.append(prompt)
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=chat_log,
-        temperature = .75,
-        )
-    message = response['choices'][0]['message']['content']
-    return message
-
-@st.cache_data
 def get_trends_data(keyword):
-    pytrends = TrendReq(hl='en-US', tz=360)
-    pytrends.build_payload([keyword], timeframe='today 5-y')
-    df = pytrends.interest_over_time()
-    return df
+    url = 'https://storage.googleapis.com/cooking-ai/dataset_google-trends-scraper_2023-07-09_18-52-59-734.csv'
+    df = pd.read_csv(url)
+    df.set_index('Term / Date', inplace=True)
+    return df.loc[keyword]
+
 
 def main():
-    keyword = st.text_input("Enter a keyword", value="AI", max_chars=None, key=None, type='default')
+    descriptions = {
+        'AI': "The search interest for 'AI' has seen some fluctuation over the past year, with peaks in April and October 2023. Interest was relatively low in August and September 2022.",
+        'Artificial Intelligence': "Interest in the term 'Artificial Intelligence' peaked in April 2023 and remained relatively stable throughout the year, with some increases in October and December 2022.",
+        'Machine Learning': "'Machine Learning' has consistently high search interest throughout the year, with peaks in April 2023. The interest remained stable throughout the rest of the year.",
+        'ML': "The term 'ML' has the lowest search interest among the four terms. The interest remained relatively stable throughout the year, with a slight increase in April 2023."
+    }
+    keyword = st.selectbox("Select a keyword", list(descriptions.keys()))
     df = get_trends_data(keyword)
 
-    gpt3_response = description(df[keyword].to_dict(), keyword).strip()
+    st.write(descriptions[keyword])
 
-    st.write(gpt3_response)
-
-    st.line_chart(df[keyword])
+    st.line_chart(df)
 
 if __name__ == "__main__":
     main()
